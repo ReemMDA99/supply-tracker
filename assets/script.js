ca83963de9dfb19eb3998f636fb9c942570242c4
Reem Ahmed <reem.mda0909@gmail.com>
2022-10-20 12:50:50 -0600

    supply tracker app

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// on click event listener
+// ready- once this document finishes loading
+$(document).ready(function(){
+$("#button").click(function() {
+    // console.log("click");
+    var campListItem = $("input[name=campItem").val();
+    $("ol").append("<li>" + campListItem + "</li>");
+    $("input[name=campItem").val("");
+});
+// double click to strike and unstrike items
+$("ol").on("dblclick", "li", function() {
+    //event delegation- allows us to attach a single event listener to a parent element that will fire for all descendents matchin selector, whether those descendants exist now or are added in the future
+    //event propogation/ event bubbling- dynamically generated- no direct event listener. when the event is clicked it travels all the way up to the parent like a bubble(bottom to top)
+    $(this).toggleClass("strike");
+})
+
+});
