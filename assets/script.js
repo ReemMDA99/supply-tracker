06738b1f3c2242c1b84f8ac53504b9038330c1ae
Reem Ahmed <reem.mda0909@gmail.com>
2022-10-20 15:07:49 -0600

    supply tracker app

--- assets/script.js
+++ assets/script.js
@@ -14,6 +14,14 @@ $("ol").on("dblclick", "li", function() {
     //event delegation- allows us to attach a single event listener to a parent element that will fire for all descendents matchin selector, whether those descendants exist now or are added in the future
     //event propogation/ event bubbling- dynamically generated- no direct event listener. when the event is clicked it travels all the way up to the parent like a bubble(bottom to top)
     $(this).toggleClass("strike");
+});
+$("ol").sortable();
+$(function(){
+$("#datepicker").datepicker({
+    onSelect: function(dateText, inst) {
+        var formattedDate = moment(dateText).format('MM Do YY');
+        $("date-text").text(formattedDate);
+    }
 })
-
+});
 });
