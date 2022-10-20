708379a270c72a35a837ddbdf4a53c25d1e6657d
Reem Ahmed <reem.mda0909@gmail.com>
2022-10-20 13:27:56 -0600

    supply tracker app

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,13 @@
 // ready- once this document finishes loading
 $(document).ready(function(){
 $("#button").click(function() {
-    // console.log("click");
+    // console.log("keypress");
     var campListItem = $("input[name=campItem").val();
     $("ol").append("<li>" + campListItem + "</li>");
     $("input[name=campItem").val("");
 });
+// Keyboard events
+
 // double click to strike and unstrike items
 $("ol").on("dblclick", "li", function() {
     //event delegation- allows us to attach a single event listener to a parent element that will fire for all descendents matchin selector, whether those descendants exist now or are added in the future
